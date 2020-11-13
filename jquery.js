93e0dc9f1a8edbfab13a6d44e0a7f38f1cb21418
rustamhajiyev94 <40638967+rustamhajiyev94@users.noreply.github.com>
2020-11-13 19:10:34 +0400

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $(".container").fadeIn(2000); 
+    $("input").focus(function(){
+        $(this).css("background-color", "#cccccc");
+      });
+
+     
+}); 
+   
+
